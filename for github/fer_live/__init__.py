7518267bc5790de7d8ed71fe5317fa58959045b2
Puneeth <subramanyapuneeth530@gmail.com>
2025-10-06 12:53:03 +0530

    Delete for github directory

--- for github/fer_live/__init__.py
+++
@@ -1,4 +0,0 @@
-from .utils import apply_torch_cpu_safety
-from .model import EmotionModel
-from .pipeline import FaceAligner
-from .gui import draw_side_panel, concat_with_panel, draw_bbox, PANEL_W
